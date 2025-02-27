b4b6c44f886ea0536967c5141d5da8be703e1f8a
Gabriel <gdenmor2001@g.educaand.es>
2025-02-27 15:03:42 +0100

    CRUD Pedido hecho. Falta el de pizzas

--- src/main/java/com/example/ejemplo/controller/PizzaController.java
+++ src/main/java/com/example/ejemplo/controller/PizzaController.java
@@ -4,6 +4,7 @@ import com.example.ejemplo.entidades.Pedido;
 import com.example.ejemplo.repositorios.PedidoRepository;
 import com.example.ejemplo.servicios.PedidoService;
 import com.example.ejemplo.servicios.PizzaService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -40,11 +41,31 @@ public class PizzaController {
         return "addPedido";
     }
 
+    @GetMapping("/editaPedidos/{username}/{id_pedido}")
+    public String creaPedidos(@ModelAttribute Pedido pedido, BindingResult bindingResult, Model model,@PathVariable String username,@PathVariable String id_pedido){
+        model.addAttribute("pizzas",this.pizzaService.findAll());
+        model.addAttribute("pedido", this.pedidoService.findPedidoById(id_pedido));
+        model.addAttribute("username",username);
+        model.addAttribute("id",id_pedido);
+        return "editPedido";
+    }
+
     @PostMapping("/addPedidos/{username}")
     public String addPedidos(@ModelAttribute Pedido pedido, BindingResult bindingResult, Model model,@PathVariable String username){
         pedido.setCliente(username);
         pedido.setFecha_pedido(new Date());
         this.pedidoService.insertaPedido(pedido);
+        return "redirect:/pedidos/"+username;
+    }
+    @PostMapping("/editPedidos/{username}/{id}")
+    public String editPedidos(@ModelAttribute("pedido") Pedido pedido, BindingResult result, Model model,@PathVariable String username,@PathVariable String id){
+        if (result.hasErrors()) {
+            return "editPedido";
+        }else {
+            pedido.setCliente(username);
+            pedido.set_id(new ObjectId(id));
+            this.pedidoService.updatePedido(pedido);
+        }
         return "redirect:/pedidos/"+username;
     }
     @GetMapping("/deletePedido/{id}/{username}")
